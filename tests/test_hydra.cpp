22f1282eafd376cb2bbb031e1f41414ee2033fc8
ot4562-glitch <ot4562@gmail.com>
2026-09-15 19:04:43 +0900

    feat: add bounded virtual XInput pipe transport

--- tests/test_hydra.cpp
+++ tests/test_hydra.cpp
@@ -14,6 +14,7 @@ void testControllerPairing();
 void testControllerVirtualXInput();
 void testVirtualXInputProtocol();
 void testVirtualXInputService();
+void testVirtualXInputPipe();
 
 void testHardwareDetector() {
     hydra::HardwareDetector detector;
@@ -226,6 +227,7 @@ int main() {
     testControllerVirtualXInput();
     testVirtualXInputProtocol();
     testVirtualXInputService();
+    testVirtualXInputPipe();
     std::cout << "All HydraSeat Engine Tests Passed!" << std::endl;
     return 0;
 }
